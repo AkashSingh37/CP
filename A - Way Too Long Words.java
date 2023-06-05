ee5be170b7fbfd9e5ef10286e4c4d7f2aaae7bd7
Akash Singh <85953541+AkashSingh37@users.noreply.github.com>
2023-06-05 23:57:41 +0530

    A

--- A - Way Too Long Words.java
+++ A - Way Too Long Words.java
@@ -1,1 +1,18 @@
-
+import java.util.Scanner;
+ 
+public class WordsMinimizedProbA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0){
+            String str = sc.next();
+            int mini = str.length()-2;
+            if(str.length()>10){
+                System.out.println(str.charAt(0)+String.valueOf(mini)+str.charAt(str.length()-1));
+            }else{
+                System.out.println(str);
+            }
+            t--;
+        }
+    }
+}
