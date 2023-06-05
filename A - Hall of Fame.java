1d65f9b2df71f700847dab2220ff56f0f1bae329
Akash Singh <85953541+AkashSingh37@users.noreply.github.com>
2023-06-05 23:56:28 +0530

    A

--- A - Hall of Fame.java
+++ A - Hall of Fame.java
@@ -1,1 +1,34 @@
-
+import java.util.Scanner;
+ 
+public class HallofFame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0){
+            int n = sc.nextInt();
+            String s = sc.next();
+            int one = 0;
+            int a = 0;
+            int b = 0;
+            for(int i=0; i<n-1; i++){
+                if(s.charAt(i) == 'L' && s.charAt(i+1) == 'R'){
+                    one++;
+                    b =i+1;
+                }
+                if(s.charAt(i) == 'R' && s.charAt(i+1) == 'L'){
+                    a++;
+                }
+            }
+            if(a>0){
+                System.out.println(0);
+            }else{
+                if(one>0){
+                    System.out.println(b);
+                }else{
+                    System.out.println(-1);
+                }
+            }
+            t--;
+        }
+    }
+}
