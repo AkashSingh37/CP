2634e089f8f4c870ae4d7c3d6fdbe10557d63698
Akash Singh <85953541+AkashSingh37@users.noreply.github.com>
2023-06-06 23:32:07 +0530

    Update A - A+B.java

--- A - A+B.java
+++ A - A+B.java
@@ -1,1 +1,13 @@
-
+import java.util.Scanner;
+ 
+public class AplusB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0){
+            String a = sc.next();
+            System.out.println(Integer.parseInt(String.valueOf(a.charAt(0)))+ Integer.parseInt(String.valueOf(a.charAt(2))));
+            t--;
+        }
+    }
+}
