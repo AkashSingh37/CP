34911a8de2c04cd3984663f62a4928b1d21ff9f1
Akash Singh <85953541+AkashSingh37@users.noreply.github.com>
2023-06-10 23:56:42 +0530

    Update A - Vasya and Coins.java

--- A - Vasya and Coins.java
+++ A - Vasya and Coins.java
@@ -1,25 +1,41 @@
-
-
+import java.util.Scanner;
+ 
+public class Vasya {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if(a==0){
+                System.out.println(1);
+            }else{
+                System.out.println(a+1+b*2);
+            }
+            t--;
+        }
+    }
+}
 // Question
 // https://codeforces.com/contest/1660/problem/A
 
 //Test Cases
-Input
-5
-1 1
-4 0
-0 2
-0 0
-2314 2374
-Output
-4
-5
-1
-1
-7063
-Answer
-4
-5
-1
-1
-7063
+// Input
+// 5
+// 1 1
+// 4 0
+// 0 2
+// 0 0
+// 2314 2374
+// Output
+// 4
+// 5
+// 1
+// 1
+// 7063
+// Answer
+// 4
+// 5
+// 1
+// 1
+// 7063
