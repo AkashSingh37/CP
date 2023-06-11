bcee6650dc3f776c81ffcac4bf9ce1916d0e8b4f
Akash Singh <85953541+AkashSingh37@users.noreply.github.com>
2023-06-11 22:46:33 +0530

    B

--- B - Vlad and Candies.java
+++ B - Vlad and Candies.java
@@ -1,4 +1,34 @@
-
+import java.util.Arrays;
+import java.util.Scanner;
+ 
+public class VladCandies {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0){
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i =0; i<n; i++){
+                arr[i] = sc.nextInt();
+            }
+            Arrays.sort(arr);
+            if(n==1){
+                if(arr[0]>1){
+                    System.out.println("NO");
+                }else{
+                    System.out.println("YES");
+                }
+            }else{
+                if(arr[n-1] - arr[n-2] >=2){
+                    System.out.println("NO");
+                }else{
+                    System.out.println("YES");
+                }
+            }
+            t--;
+        }
+    }
+}
 
 //Question
 // https://codeforces.com/contest/1660/problem/B
